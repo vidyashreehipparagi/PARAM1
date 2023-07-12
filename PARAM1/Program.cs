f23ab0849d6025aa9e37b5f65a072a2dd68a0755
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-12 16:04:09 +0530

    Add project files.

---
+++ PARAM1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARAM1
+{
+    internal class Student
+    {
+        public void AcceptNames(params string[] names)
+        {
+            Console.WriteLine("Count "+names.Length);
+            foreach(var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+         Student std = new Student();
+            std.AcceptNames("Aniket");
+            std.AcceptNames("Vidyashree", "Anjali");
+            std.AcceptNames("Akshata", "Fahad", "Gorav");
+            std.AcceptNames("Snehal", "Benzeer", "Pankaj", "sagar");
+            std.AcceptNames("Sagar", "Nilesh", "Girish", "Sanket", "Rahul");
+        }
+    }
+}
